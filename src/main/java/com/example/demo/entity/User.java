39ac629cb19dce8325e3fec44bbddfeef95ec79d
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-05-05 21:39:10 +0530

    Added rest api message

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+public class User {
+    /**
+     * Created an entity class
+     */
+
+    /**
+     * Created variables
+     */
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Created getter setters
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
